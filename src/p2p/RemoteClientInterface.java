1636773e01f158b6e87ffe59b2cc06a7f654c649
migueltb01 <migueltb01@gmail.com>
2018-04-30 13:20:17 +0200

    Arreglado erro security manager

--- src/p2p/RemoteClientInterface.java
+++ src/p2p/RemoteClientInterface.java
@@ -3,12 +3,16 @@ package p2p;// Packages
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
-import java.sql.SQLException;
 
 // Remote P2P client interface
 public interface RemoteClientInterface extends Remote {
 
-    void notifyOnline(String username);
-    void requestFriendship(String requester);
+    void notifyOnline(String username) throws RemoteException;
+
+    void notifyOffline(String username) throws RemoteException;
+
+    void requestFriendship(String requester) throws RemoteException;
+
+    void endChat(String username) throws RemoteException;
 
 }
